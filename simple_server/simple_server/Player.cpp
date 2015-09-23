43e0d44367c17e04aee3eba96f294a1d9a9eecf7
philomuzzi <gxlee1986@gmail.com>
2015-09-23 23:35:55 +0800

    some ugly code

--- simple_server/simple_server/Player.cpp
+++ simple_server/simple_server/Player.cpp
@@ -1,6 +1,8 @@
 #include "Player.h"
 #include <iostream>
 
+using namespace std;
+
 void Player::do_write(std::size_t length) {
 	auto self(shared_from_this());
 	boost::asio::async_write(
@@ -14,15 +16,41 @@ void Player::do_write(std::size_t length) {
 	);
 }
 
+void Player::start() {
+	do_read();
+}
+
+// 读取数据之后，解包，然后相应处理
+// 消息格式是: 0xFFFF0000|0x0000FFFF,一个int32，其中前16位表示消息id，后面16位表示消息整体长度。后面跟着的是protobuf压缩过的数据
 void Player::do_read() {
 	auto self(shared_from_this());
 	socket_.async_read_some(
 		boost::asio::buffer(data_, max_length),
 		[this, self](boost::system::error_code ec, std::size_t length) {
 			if (!ec) {
-				std::string msg(data_, length);
-				std::cout << msg;
-				do_write(length);
+				while (data_.size() > HeadLength) {
+					const char* _data = data_.data();
+					int head = *(int *)_data;
+					std::cout << "Head length: " << head;
+					int msgid = head >> 16;
+					int len = head & MSGLENPACKET;
+					if (len > max_length || len < 0) {
+						std::cerr << "头长度错误" << endl;
+						data_ = {};
+					} else if (data_.size() >= len) {
+						auto begin = data_.begin();						
+						std::array<char, max_length> content;
+						std::copy(begin + 3, begin + 3 + len, content.begin());
+						std::copy(begin + 3 + len, data_.end(), begin);	// 后面的数据没有清空
+
+						//将msgid和content放入消息处理函数中
+					}
+
+				}
+//				std::cout << std::string(data_.data(), length);
+//				do_write(length);
+
+				do_read();
 			}
 		}
 	);
--- simple_server/simple_server/Player.h
+++ simple_server/simple_server/Player.h
@@ -3,21 +3,22 @@
 #include <memory>
 #include <boost/asio.hpp>
 
+#define HeadLength		4
+#define MSGIDPACKET		0x7fff0000
+#define MSGLENPACKET	0x0000ffff
+
 class Player : public std::enable_shared_from_this<Player> {
 public:
 	Player(boost::asio::ip::tcp::socket socket) : socket_(std::move(socket)) {}
 
-	void start() {
-		do_read();
-	}
+	void start();
 
 private:
 	void do_read();
 	void do_write(std::size_t length);
 
-
 	boost::asio::ip::tcp::socket socket_;
-	enum {max_length = 1024};
-	char data_[1024];
+	enum {max_length = 65535};
+	std::array<char, max_length> data_;
 };
 
