b906174979e48c6044c83152bfcca3d886bc00f6
philomuzzi <gxlee1986@gmail.com>
2015-10-16 16:16:53 +0800

    now ok. forget to start server, waste too much time.

--- simple_server/debug_client/main.cpp
+++ simple_server/debug_client/main.cpp
@@ -3,6 +3,7 @@
 #include "ConnectionMsgCenter.h"
 #include "../Utility/define.h"
 #include "Connection.h"
+#include <thread>
 
 int main() {
 	try {
@@ -10,10 +11,15 @@ int main() {
 
 		boost::asio::io_service io_service;	
 		boost::asio::ip::tcp::endpoint ep(boost::asio::ip::address::from_string("127.0.0.1"), ServerPort);
-		auto conn = std::make_shared<Connection>(io_service);
-		conn->start(ep, "123");
 
-		io_service.run();
+		std::thread t([&]{
+			auto conn = std::make_shared<Connection>(io_service);
+			conn->start(ep, "123");
+
+			io_service.run();
+		});
+
+		t.join();
 	}
 	catch (std::exception& e) {
 		std::cerr << "Exception: " << e.what() << "\n";
